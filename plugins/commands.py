8985b503e0b8465f6c1e81edb0125fdbbab6ab49
MMW BOTZ <akhilkrishnausa50@gmail.com>
2025-12-12 19:05:43 +0530

    Change log file name from TelegramBot.txt to TelegramBot.log

--- plugins/commands.py
+++ plugins/commands.py
@@ -191,7 +191,7 @@ async def channel_info(bot, message):
 async def log_file(bot, message):
     """Send log file"""
     try:
-        await message.reply_document('TelegramBot.txt')
+        await message.reply_document('TelegramBot.log')
     except Exception as e:
         await message.reply(str(e))
 
